21e67e342cbbfba28698a405645c1543e0421782
Maxim Lebiga <lebiga.maks@gmail.com>
2021-05-29 12:08:19 +0300

    task

--- js/task.js
+++ js/task.js
@@ -4,8 +4,10 @@ const galleryListEl = document.querySelector('.gallery')
 const modalEl = document.querySelector('.lightbox')
 const modalImgEl = document.querySelector('.lightbox__image')
 const modalButtonEl = document.querySelector('.lightbox__button')
+let currentModalImgIndex
 
-const createGalleryElementMarkup = function (elem)  {
+
+const createGalleryElementMarkup = function (elem) {
     return `
     <li class="gallery__item">
         <a
@@ -23,11 +25,25 @@ const createGalleryElementMarkup = function (elem)  {
     `
 }
 
+
 const createGalleryListMarkup = function (array) {
     return array.map(createGalleryElementMarkup).join('')
 }
 
+
 galleryListEl.innerHTML = createGalleryListMarkup(defaultExport)
+
+
+const findCurrentModalImgIndex = function (array) {
+    let modalImgIndex
+    array.forEach((elem, index) => {
+        if (elem.original === modalImgEl.src) {
+            modalImgIndex = index
+        }
+    })
+
+    return modalImgIndex 
+}
 
 
 const onImgClick = function (event) {
@@ -40,8 +56,10 @@ const onImgClick = function (event) {
     modalEl.classList.add('is-open')
     modalImgEl.src = event.target.dataset.source
     modalImgEl.alt = event.target.alt
+    currentModalImgIndex = findCurrentModalImgIndex(defaultExport)
 
 }
+
 
 const onModalButtonClick = function () {
     modalEl.classList.remove('is-open')
@@ -49,15 +67,69 @@ const onModalButtonClick = function () {
     modalImgEl.alt = ''
 }
 
-const onEscKeyPress = function (event) {
-    if (modalEl.classList.contains('is-open')) {
-        if (event.code === 'Escape') {
-            modalEl.classList.remove('is-open')
-        }
+
+const onEscButtonPress = function (event) {
+    if (modalEl.classList.contains('is-open') && event.code !== 'Escape') {
+        return
     }
+
+    modalEl.classList.remove('is-open')
+    modalImgEl.src = ''
+    modalImgEl.alt = ''
 }
 
 
+const changeModalImgByRightButtonPress = function (event) {
+    if (modalEl.classList.contains('is-open') && event.code === 'ArrowRight') {
+        const nextModalImgindex = currentModalImgIndex + 1
+        if (nextModalImgindex < defaultExport.length) {
+            currentModalImgIndex += 1
+
+            modalImgEl.src = defaultExport[nextModalImgindex].original
+            modalImgEl.alt = defaultExport[nextModalImgindex].description
+        } else {
+            modalImgEl.src = defaultExport[0].original
+            modalImgEl.alt = defaultExport[0].description
+
+            currentModalImgIndex = 0
+        }
+    }
+
+}
+
+
+const changeModalImgByLeftButtonPress = function (event) {
+    if (modalEl.classList.contains('is-open') && event.code === 'ArrowLeft') {
+        const nextModalImgindex = currentModalImgIndex - 1
+        if (nextModalImgindex < 0) {
+            modalImgEl.src = defaultExport[defaultExport.length - 1].original
+            modalImgEl.alt = defaultExport[defaultExport.length - 1].description
+
+            currentModalImgIndex = defaultExport.length - 1
+        } else {
+            currentModalImgIndex -= 1
+
+            modalImgEl.src = defaultExport[nextModalImgindex].original
+            modalImgEl.alt = defaultExport[nextModalImgindex].description
+        }
+    }
+
+}
+
+const onBlackPartModalElClick = function (event) {
+    if (event.target.nodeName !== 'DIV') {
+        return
+    }
+
+    modalEl.classList.remove('is-open')
+    modalImgEl.src = ''
+    modalImgEl.alt = ''    
+}
+
+
+modalEl.addEventListener('click', onBlackPartModalElClick)
 galleryListEl.addEventListener('click', onImgClick)
 modalButtonEl.addEventListener('click', onModalButtonClick)
-document.addEventListener('keydown', onEscKeyPress)
+document.addEventListener('keydown', onEscButtonPress)
+document.addEventListener('keydown', changeModalImgByRightButtonPress)
+document.addEventListener('keydown', changeModalImgByLeftButtonPress)
